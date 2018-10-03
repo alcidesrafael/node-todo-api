39c4b349c66a9dd46fd9fd9ef6b13bad8b8805eb
alcidesrafael <rafael4left@gmail.com>
2018-10-03 14:35:56 -0300

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -205,13 +205,17 @@ describe('PATCH /todos/:id', () => {
 
 describe('GET /users/me', () => {
     it('should return user if authenticated', (done) => {
+        var token = users[0].tokens[0].token;
+        var hexId = users[0]._id.toHexString();
+        var email = users[0].email;
+
         request(app)
             .get('/users/me')
-            .set('x-auth', users[0].tokens[0].token)
+            .set('x-auth', token)
             .expect(200)
             .expect((res) => {
-                expect(res.body._id).toBe(users[0]._id.toHexString());
-                expect(res.body.email).toBe(users[0].email);
+                expect(res.body._id).toBe(hexId);
+                expect(res.body.email).toBe(email);
             })
             .end(done);
     });
@@ -250,7 +254,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
-                });
+                }).catch((e) => done(e));
             });
     });
 
@@ -276,3 +280,58 @@ describe('POST /users', () => {
             .end(done);
     });
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        var hexId = users[1]._id.toHexString();
+        var email = users[1].email;
+        var password = users[1].password;
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+                expect(res.body._id).toBe(hexId);
+                expect(res.body.email).toBe(email);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(hexId).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should reject invalid login', (done) => {
+        var hexId = users[1]._id.toHexString();
+        var email = users[1].email;
+        var password = users[1].password + '1';
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(hexId).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+})
